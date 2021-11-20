2df77088a190c2f06f059a3d84408748278d74b1
mariopreis86 <mariopimenta86@gmail.com>
2021-11-20 19:28:50 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/mariopimenta/cursomc/CursomcApplication.java
+++ src/main/java/com/mariopimenta/cursomc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mariopimenta.cursomc.domain.Categoria;
+import com.mariopimenta.cursomc.domain.Produto;
 import com.mariopimenta.cursomc.repositories.CategoriaRepository;
+import com.mariopimenta.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -25,7 +30,19 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(List.of(p1, p2, p3));
+		cat2.getProdutos().addAll(List.of(p2));
+		
+		p1.getCategorias().addAll(List.of(cat1));
+		p2.getCategorias().addAll(List.of(cat1, cat2));
+		p3.getCategorias().addAll(List.of(cat1));
+		
 		categoriaRepository.saveAll(List.of(cat1, cat2));
+		produtoRepository.saveAll(List.of(p1, p2, p3));
 	}
 
 }
--- src/main/java/com/mariopimenta/cursomc/domain/Categoria.java
+++ src/main/java/com/mariopimenta/cursomc/domain/Categoria.java
@@ -1,22 +1,27 @@
 package com.mariopimenta.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
-
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {}
 	public Categoria(Integer id, String nome) {
@@ -53,5 +58,13 @@ public class Categoria implements Serializable {
 			return false;
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
+	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 }
