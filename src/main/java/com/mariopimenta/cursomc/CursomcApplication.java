7fe7524807def1c9e8779a560724d25edf5203ea
mariopreis86 <mariopimenta86@gmail.com>
2021-11-20 17:54:09 -0300

    Criando operacao de instancia

--- src/main/java/com/mariopimenta/cursomc/CursomcApplication.java
+++ src/main/java/com/mariopimenta/cursomc/CursomcApplication.java
@@ -1,13 +1,31 @@
 package com.mariopimenta.cursomc;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mariopimenta.cursomc.domain.Categoria;
+import com.mariopimenta.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(List.of(cat1, cat2));
 	}
 
 }
