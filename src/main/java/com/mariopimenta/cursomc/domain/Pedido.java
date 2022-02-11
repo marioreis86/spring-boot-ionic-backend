68934d575220d443c18bed82cc22574277a8bb0d
mariopreis86 <mariopimenta86@gmail.com>
2022-02-10 22:16:00 -0300

    Subtotal e total do Pedido

--- src/main/java/com/mariopimenta/cursomc/domain/Pedido.java
+++ src/main/java/com/mariopimenta/cursomc/domain/Pedido.java
@@ -57,4 +57,12 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
+	public double getValorTotal() {
+		double soma = 0.0;
+		for (ItemPedido ip : itens) {
+			soma += ip.getSubTotal();
+		}
+		return soma;
+	}
 }
